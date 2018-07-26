e9b801e19251adeb5614801b6276fa9793e08016
XDTG <_tg@clickdeMacBook-Pro.local>
2018-07-27 00:09:59 +0800

    json

--- 1.0_beta/app/src/main/java/com/example/tg/wifi_mapbaeta10/gaoDeMap.java
+++ 1.0_beta/app/src/main/java/com/example/tg/wifi_mapbaeta10/gaoDeMap.java
@@ -20,13 +20,19 @@ import com.amap.api.maps2d.model.Marker;
 import com.amap.api.maps2d.model.MarkerOptions;
 import com.amap.api.maps2d.model.MyLocationStyle;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 
 public class gaoDeMap extends Activity {
 
     MapView mMapView = null;
     int width,height;
-
+    String json_str;
+    double mark_lat,mark_lng;
+    String mark_essid,mark_bssid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,8 +81,27 @@ public class gaoDeMap extends Activity {
                 Marker markerr = null;
                 LatLng mkr = adjustCamerar(aMap,height,width);
                 LatLng mkl = adjustCameral(aMap);
-                markerr = aMap.addMarker((new MarkerOptions().position(mkr).title("edger").snippet(String.valueOf(mkr.latitude)+"\n"+String.valueOf(mkr.longitude))));
-                markerl = aMap.addMarker((new MarkerOptions().position(mkl).title("edgel").snippet(String.valueOf(mkl.latitude)+"\n"+String.valueOf(mkl.longitude))));
+                json_str = "[{\"essid\":\"LW\",\"channel\":11,\"bssid\":\"f4:6a:92:6e:66:b9\",\"lantitude\":35.173055,\"longitude\":109.940483},{\"essid\":\"TP-LINK_88888\",\"channel\":11,\"bssid\":\"d8:15:0d:2d:32:ba\",\"lantitude\":35.173567,\"longitude\":109.940383},{\"essid\":\"HUAWEI-69p8\",\"channel\":11,\"bssid\":\"04:9f:ca:6c:12:e0\",\"lantitude\":35.173655,\"longitude\":109.940283},{\"essid\":\"TP-LINK_C436\",\"channel\":6,\"bssid\":\"f4:83:cd:de:c4:36\",\"lantitude\":35.173755,\"longitude\":109.940483},{\"essid\":\"xiaomi\",\"channel\":6,\"bssid\":\"f0:b4:29:1f:9d:1d\",\"lantitude\":35.173055,\"longitude\":109.940783},{\"essid\":\"TP-LINK_ranran\",\"channel\":6,\"bssid\":\"b8:f8:83:bd:43:cd\",\"lantitude\":35.173055,\"longitude\":109.940183},{\"essid\":\"FAST_02DFCC\",\"channel\":2,\"bssid\":\"78:eb:14:02:df:cc\",\"lantitude\":35.172055,\"longitude\":109.940483},{\"essid\":\"MERCURY_250BAA\",\"channel\":2,\"bssid\":\"d8:15:0d:25:0b:aa\",\"lantitude\":35.173955,\"longitude\":109.940483},{\"essid\":\"CMCC-nr7k\",\"channel\":2,\"bssid\":\"ac:f9:70:94:69:90\",\"lantitude\":35.173095,\"longitude\":109.940483},{\"essid\":\"\",\"channel\":2,\"bssid\":\"b4:0f:3b:c7:00:01\",\"lantitude\":35.173055,\"longitude\":109.940493}]";
+                try{
+                    JSONArray jsonArray = new JSONArray(json_str);
+                    for(int i =0;i<jsonArray.length();i++){
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        mark_essid = jsonObject.getString("essid");
+                        mark_bssid = jsonObject.getString("bssid");
+                        mark_lat = jsonObject.getDouble("lantitude");
+                        mark_lng = jsonObject.getDouble("longitude");
+
+                        LatLng latLng_mark = new LatLng(mark_lat,mark_lng);
+                        Marker marker = aMap.addMarker(new MarkerOptions().position(latLng_mark).title(mark_essid).snippet(mark_bssid));
+
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                //边缘点
+                //markerr = aMap.addMarker((new MarkerOptions().position(mkr).title("edger").snippet(String.valueOf(mkr.latitude)+"\n"+String.valueOf(mkr.longitude))));
+                //markerl = aMap.addMarker((new MarkerOptions().position(mkl).title("edgel").snippet(String.valueOf(mkl.latitude)+"\n"+String.valueOf(mkl.longitude))));
             }
         });
 
